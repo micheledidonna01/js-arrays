f70a25954a20512c99740ef33fef3602722bdcce
micheledidonna01 <micheledidonna337@gmail.com>
2025-02-26 15:24:08 +0100

    feat: resolved 01-arrays

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,24 +10,57 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[4];
+console.log('Esercizio 1: ',fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
 
+for (i = 0; i < teachers.length; i++) {
+  if (i === 4) {
+    teachers[i] = 'Patrick';
+    console.log('Esercizio 2: ', teachers[i]);
+  }
+}
+
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
+console.log('Esercizio 3:')
+console.log(lastTeacher);
+console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log('Esercizio 4:')
+console.log(firstTeacher);
+console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push('Vanessa');
+console.log('Esercizio 5: ', teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift('Sarah');
+console.log('Esercizio 6: ', teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+
+for(i=0; i<teachers.length; i++){
+  if(teachers[i] === 'Lewis'){
+    const lewisIndex = i;
+    console.log('Esercizio 7: L\'indice è', lewisIndex);
+  }
+}
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+
+const isTeachersEmpty = teachers.length;
+
+  if(isTeachersEmpty > 0){
+    console.log('L\'array è pieno');
+  }else{
+    console.log('L\'array è vuoto');
+  }
+
+console.log(`Esercizio 8: l'array contiene: ${isTeachersEmpty} elementi`);
